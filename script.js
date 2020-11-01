d893d8e5454c82325476da8b3110179f21a4c8a6
schawn6484 <schawnetta.robinson@gmail.com>
2020-10-31 23:13:39 -0400

    modified javacript with local storage and array

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ $(document).ready(function(){
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
     var currentHour = moment().hour();
 
+    var savedPlan = JSON.parse(localStorage.getItem("savedPlan"));
+
+    if(savedPlan !== null){
+        planTextArray =savedPlan
+    }
+
+    for( var hour = 9; hour <= 17; hour++)
+
+
     $('.time-block').each(function(){
     var id = parseInt($(this).attr('id'));
     if(id < currentHour){
@@ -19,22 +28,7 @@ $(document).ready(function(){
     
     )
 
-    $('#saveBtn').click(function(){
 
-        
-    }
-    
-    
-    
-    
-    )
-    
-    localStorage.setItem(" " ,  JSON.stringify()); 
-
-
-
-
-
-
+localStorage.setItem("savedPlan", JSON.stringify(planTextArray)); 
 
 });
